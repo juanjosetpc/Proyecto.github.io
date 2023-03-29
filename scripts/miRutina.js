d57a9fa59c0eee920cb9618263cd33e5f74dec98
Javiserranoc <javiserranoc@uma.es>
2023-03-29 16:54:37 +0200

    Pequeños cambios en javascript de misRutinas

--- scripts/miRutina.js
+++ scripts/miRutina.js
@@ -1,40 +1,38 @@
-const titulo = document.getElementById("misRutinas");
-const titulo2 = document.getElementById("rutinasFav");
+const titulo = document.getElementById("tituloMisRutinas");
+const titulo2 = document.getElementById("tituloMisFavoritas");
 titulo.style.cursor = "pointer";
 titulo2.style.cursor = "pointer";
 var i = 0;
 var i2 = 0;
 
 function abrir(){
+    const padre = document.getElementById("misRutinas");
     if(i == 0){
         i++;
-        titulo.style.marginBottom = "30%";
-        titulo.style.height = "30%";
-        var tituloMisFavoritas = document.querySelector("#tituloMisRutinas");
-        tituloMisFavoritas.innerHTML = "Mis Rutinas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-up' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z'/></svg>";
+        padre.style.marginBottom = "30%";
+        padre.style.height = "30%";
+        titulo.innerHTML = "Mis Rutinas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-up' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z'/></svg>";
     }else{
         i--;
-        titulo.style.marginBottom = "0.2%";
-        titulo.style.height = "0%";
-        var tituloMisFavoritas = document.querySelector("#tituloMisRutinas");
-        tituloMisFavoritas.innerHTML = "Mis Rutinas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-down' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/></svg>";
+        padre.style.marginBottom = "0.2%";
+        padre.style.height = "0%";
+        titulo.innerHTML = "Mis Rutinas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-down' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/></svg>";
     }
 }
 
 function abrir2(){
+    const padre = document.getElementById("rutinasFav");
     if(i2 == 0){
         i2++;
-        titulo2.style.marginBottom = "30%";
-        titulo2.style.height = "30%";
-        var tituloMisFavoritas = document.querySelector("#tituloMisFavoritas");
-        tituloMisFavoritas.innerHTML = "Mis Favoritas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-up' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z'/></svg>";
+        padre.style.marginBottom = "30%";
+        padre.style.height = "30%";
+        titulo2.innerHTML = "Mis Favoritas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-up' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z'/></svg>";
 
     }else{
         i2--;
-        titulo2.style.marginBottom = "0%";
-        titulo2.style.height = "0%";
-        var tituloMisFavoritas = document.querySelector("#tituloMisFavoritas");
-        tituloMisFavoritas.innerHTML = "Mis Favoritas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-down' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/></svg>";
+        padre.style.marginBottom = "0%";
+        padre.style.height = "0%";
+        titulo2.innerHTML = "Mis Favoritas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-down' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/></svg>";
     }
 }
 
