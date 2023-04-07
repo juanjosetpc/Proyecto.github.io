2e226abc21a1b240d7787bb12a44be1ba27ddd7e
Javiserranoc <javiserranoc@uma.es>
2023-04-07 18:36:18 +0200

    Cambio id a class

--- scripts/miRutina.js
+++ scripts/miRutina.js
@@ -6,28 +6,26 @@ var i = 0;
 var i2 = 0;
 
 function abrir(){
-    const miRutina = document.getElementById("miRutina");
     if(i == 0){
         i++;
-        miRutina.style.display = "block";
+        document.querySelectorAll(".miRutina").forEach(a=>a.style.display = "block");
         titulo.innerHTML = "Mis Rutinas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-up' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z'/></svg>";
     }else{
         i--;
-        miRutina.style.display = "none";
+        document.querySelectorAll(".miRutina").forEach(a=>a.style.display = "none");
         titulo.innerHTML = "Mis Rutinas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-down' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/></svg>";
     }
 }
 
 function abrir2(){
-    const rutinaFav = document.getElementById("rutinaFav");
     if(i2 == 0){
         i2++;
-        rutinaFav.style.display = "block";
+        document.querySelectorAll(".rutinaFav").forEach(a=>a.style.display = "block");
         titulo2.innerHTML = "Mis Favoritas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-up' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z'/></svg>";
 
     }else{
         i2--;
-        rutinaFav.style.display = "none";
+        document.querySelectorAll(".rutinaFav").forEach(a=>a.style.display = "none");
         titulo2.innerHTML = "Mis Favoritas <svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='currentColor' class='bi bi-chevron-down' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/></svg>";
     }
 }
