1be0b323afcf039d8837c146e9bca31b29a8177a
Nicolas99ReyesTrujillo <extr4nicozz1@gmail.com>
2023-04-11 11:37:39 +0200

    Falta añadir consejos y errores comunes.

--- scripts/ejercicios.js
+++ scripts/ejercicios.js
@@ -1,25 +1,24 @@
 $(document).ready(function(){
-    //En teoría con esto deberia cambiar el color de All que es predefinido pero no lo hace
-    $('.category-list .categoryItem[category = "all"]').addClass('ct_item-active');
+    //Cambiar el color de All que es predefinido
+    $('.category-list .categoryItem[category = "all"]').addClass('ct-item-active');
 
     $('.categoryItem').click(function(){
         //Guardo variable de categoria que haga click
         var catEjer = $(this).attr('category');
         console.log(catEjer);
-        //Cambiar color al seleccionado (no lo hace)
-        $('.categoryItem').removeClass('ct_item-active');
-        $(this).addClass('ct_item-active');
+        //Cambiar color al seleccionado
+        $('.categoryItem').removeClass('ct-item-active');
+        $(this).addClass('ct-item-active');
         //Oculto productos
-        $('.ejer-item').hide();
+        $('.col').hide();
         //Muestro productos de categoria seleccionada
-        $('.ejer-item[category = "'+catEjer+'"]').show();
+        $('.col[category = "'+catEjer+'"]').show();
 
     });
     //Muestra todos los productos
     $('.categoryItem[category ="all"]').click(function(){
-        $('.ejer-item').show();
+        $('.col').show();
     });
-
 });
 
 //Conecto el json con el html y me carga todos los objetos
@@ -27,14 +26,43 @@ var app ={};
 var miCallback = datos =>{
     app.listaEjercicios=datos;
     var html=""
+    html+= "<div class='container-fluid'>";
+        html+= "<div class='row pt-5'>";
+        html+= "<div class='col-12'>";
+        html+= "<h3 class='text-uppercase border-bottom mb-4'>Coleccion de ejercicios</h3>";
+        html+= "</div>";
+        html+= "</div>";
+        html+= "<div class='row'>";
     app.listaEjercicios.map(ejercicio => { 
-        html+= "<div class = 'ejer-item' category = '"+ejercicio.tipoMuscular+"'>";
-        html+= "<h3>"+ejercicio.nombre+"</h3>";
-        html+= "</br>";
-        html+= "<p>"+ejercicio.tipoMuscular+"</p>";
-        html+= "<p>"+ejercicio.descripcion+"</p>";
-        html+= "<p>"+ejercicio.series+"/"+ ejercicio.repeticiones+"</p>"
-        html+= "<img src ='"+ejercicio.img+"'></img>";
+        html+= "<div class='col-lg-4 mb-3 d-flex align-items-stretch' category ='"+ejercicio.tipoMuscular+"'>";
+        html+= "<div class='col' category ='"+ejercicio.tipoMuscular+"'>";
+        html+= "<div class='card h-100'>";
+        html+= "<img src='"+ejercicio.img+"' class='card-img-top' alt='Card Image'></img>";
+        html+= "<div class='card-body d-flex flex-column'>"
+        html+= "<h5 class='card-title'>"+ejercicio.nombre+"</h5>";
+        html+= "<p class='card-text mb-4'>"+ejercicio.descripcion+"</p>";
+        html+= "<button type='button' class='btn btn-primary mt-auto align-self-start' data-bs-toggle='modal' data-bs-target='#miModal'>Saber Mas</button>";
+       
+        html+= "<div class='modal fade' id='miModal' tabindex='-1' aria-hidden='true' aria-labelledby='modalTitle'>";
+        html+= "<div class='modal-dialog'>";
+        html+= "<div class='modal-content'>";
+        html+= "<div class='modal-header'>";
+        html+= "<h5 class='modal-title' id='modalTitle'>Consejos y Errores Comunes</h5>";
+        html+= "<button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>";
+        html+= "</div>";
+        html+= "<div class='modal-body'>";
+        html+= "<p>En este apartado añadiremos consejos y errores comunes</p>";
+        html+= "</div>";
+        html+= "<div class='modal-footer'>";
+        html+= "<button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>";
+        html+= "</div>";
+        html+= "</div>";
+        html+= "</div>";
+        html+= "</div>";
+
+        html+= "</div>";
+        html+= "</div>";
+        html+= "</div>";
         html+= "</div>";
     })
     document.getElementById("listado-ejercicios").innerHTML = html;
