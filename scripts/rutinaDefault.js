f35e7a4eb48aa281801c41b7ee2b3f2b007a1d31
Javiserranoc <javiserranoc@uma.es>
2023-04-12 19:59:58 +0200

    filtro pocho y arreglo del style en html

--- scripts/rutinaDefault.js
+++ scripts/rutinaDefault.js
@@ -36,7 +36,6 @@ const path = favo.querySelector('path');
 
 favo.addEventListener("click", function() {
   const favoritas = JSON.parse(localStorage.getItem(logeado + 'favoritas'));
-  
   if (!favoritas.includes(idRutina)) {
     favoritas.push(idRutina);
     favoritas.sort();
@@ -54,7 +53,7 @@ favo.addEventListener("click", function() {
     favoritas.sort();
     localStorage.setItem(logeado + 'favoritas', JSON.stringify(favoritas));
     path.setAttribute('d', 'm8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z');
-    
+
     $(favo).popover({
       content: 'Eliminado de mis rutinas',
       placement: 'top',
