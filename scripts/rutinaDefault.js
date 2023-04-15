9ac57cd07e385b947a34c8535a9ee9d3179d1606
Antonio <antonionavajaortega@uma.es>
2023-04-15 18:59:00 +0200

    popover

--- scripts/rutinaDefault.js
+++ scripts/rutinaDefault.js
@@ -41,7 +41,7 @@ favo.addEventListener("click", function() {
     favoritas.sort();
     localStorage.setItem(logeado + 'favoritas', JSON.stringify(favoritas));
     path.setAttribute('d', 'M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z');
-
+    
     $(favo).popover({
       content: 'Añadido a mis rutinas favoritas',
       placement: 'top',
@@ -63,7 +63,10 @@ favo.addEventListener("click", function() {
   }
 
   $(favo).popover('show');
-
+  setTimeout(function() {
+    $(favo).popover('dispose');
+  }, 1200);
+  
 });
 
 //--------------------------------------------------------------------------------------------
