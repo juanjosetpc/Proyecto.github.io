3595f7cde857e807c0b89e7e3036c3db733d825d
juanjose <juanjosetp02@uma.es>
2023-05-01 00:34:49 +0200

    prueba

--- scripts/mostrarRutina.js
+++ scripts/mostrarRutina.js
@@ -6,7 +6,8 @@ function miCallback(data) {
 }
 // Hacer la solicitud JSONP
 const script = document.createElement('script');
-script.src = '/ficheros%20json/listaEjercicios.jsonp';
+script.src = "https://juanjosetpc.github.io/Proyecto.github.io/ficheros%20json/listaEjercicios.jsonp"
+// script.src = '/ficheros%20json/listaEjercicios.jsonp';
 script.async = true;
 document.head.appendChild(script);
 
