d2e511f7002f4572024c6ad6b85b8e3ce8532252
Nicolas99ReyesTrujillo <extr4nicozz1@gmail.com>
2023-05-14 03:05:32 +0200

    Versión con buscador de ejercicios

--- scripts/ejercicios.js
+++ scripts/ejercicios.js
@@ -19,7 +19,29 @@ $(document).ready(function(){
     $('.categoryItem[category ="all"]').click(function(){
         $(".col-lg-4.mb-3.align-items-stretch").show();
     });
+
+    //Scrip buscador con jQuery
+    $('#buscador').keyup(function(){
+        var nombres = $('.card-title');
+        var buscando = $(this).val();
+        var item = '';
+        for (var i = 0; i < nombres.length; i++){
+            item = $(nombres[i]).html().toLowerCase();
+            for(var x = 0; x < item.length; x++){
+                if(buscando.length == 0 || item.indexOf(buscando) > -1){
+                    $(nombres[i]).parents(".col-lg-4.mb-3.align-items-stretch").show();
+                } else {
+                    $(nombres[i]).parents(".col-lg-4.mb-3.align-items-stretch").hide();
+                }
+            }
+        }
+    });
+
 });
+
+
+
+
 
 //Conecto el json con el html y me carga todos los objetos
 var app ={};
@@ -27,11 +49,17 @@ var miCallback = datos =>{
     app.listaEjercicios=datos;
     var html=""
     html+= "<div class='container-fluid'>";
+    //Buscador
         html+= "<div class='row pt-5'>";
         html+= "<div class='col-12'>";
         html+= "<h2 class='text-uppercase border-bottom mb-4'>Coleccion de ejercicios</h2>";
+        html+= "<div class='input-group mb-4'>";
+        html+= "<input id='buscador' type='search' class='form-control rounded' placeholder='Escribe un ejercicio' aria-label='Escribe un ejercicio para buscar' aria-describedby='search-addon'/>";
+        html+= "<button type='button' class='btn btn-outline-primary' aria-label='botón buscar'>Buscar</button>";
         html+= "</div>";
         html+= "</div>";
+
+    //Html para cargar los ejercicios en cards
         html+= "<div class='row'>";
     app.listaEjercicios.map(ejercicio => { 
         html+= "<div class='col-lg-4 mb-3 align-items-stretch' category ='"+ejercicio.tipoMuscular+"'>";
@@ -42,7 +70,7 @@ var miCallback = datos =>{
         html+= "<p class='card-text mb-4'>Dificultad</p>";
         html+= "<img src='"+ejercicio.img+"' class='card-img-top' alt='Imagen de "+ejercicio.nombre+"'></img>";
         html+= "<button type='button' class='btn btn-primary mt-auto align-self-start' data-bs-toggle='modal' data-bs-target='#"+ejercicio.modalID+"'>Saber Mas</button>";
-       
+       //Html para cada modal
         html+= "<div class='modal fade' id='"+ejercicio.modalID+"' tabindex='-1' aria-hidden='true' aria-labelledby='modalTitle"+ejercicio.modalID+"'>";
         html+= "<div class='modal-dialog'>";
         html+= "<div class='modal-content'>";
